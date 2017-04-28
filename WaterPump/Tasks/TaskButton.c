d8bb701890c451562e9f1c2bf4e94b829d306e59
Patrizio Cannoni <patrizio.cannoni@gmail.com>
2017-04-27 22:28:18 -0300

    Good debouncing with INT0 disable

--- WaterPump/Tasks/TaskButton.c
+++ WaterPump/Tasks/TaskButton.c
@@ -19,11 +19,11 @@ static void prvButtonTask(void *arg);
 static xSemaphoreHandle xSemaphore;
 
 void xStartButtonTask(void) {
-	DDRD &= ~(_BV(DDD2));     	// Clear the PD2 pin
-	// PD2 (PCINT0 pin) is now an input
+	DDRD &= ~(_BV(DDD2));     	// Clear the PD2 pin.
+	// PD2 (PCINT0 pin) is now an input.
 
-	PORTD |= _BV(PORTD2);  	// turn On the Pull-up
-	// PD2 is now an input with pull-up enabled
+	PORTD |= _BV(PORTD2);  		// turn On the Pull-up.
+	// PD2 is now an input with pull-up enabled.
 
 	EICRA = 0x02;				// The falling edge of INT0 generates an interrupt request.
 	EIMSK = 0x01;				// Turns on INT0.
@@ -39,14 +39,21 @@ static void prvButtonTask(void *arg) {
 	for (;;) {
 		xSemaphoreTake(xSemaphore, portMAX_DELAY);
 
-		vTaskDelay(50);
+		// Some ms for debouncing.
+		vTaskDelay(25);
 
 		if (BUTTON_D2_PRESSED()) {
 			togglePump();
 		}
+
+		EIMSK = 0x01;			// Turns on INT0.
 	}
 }
 
 ISR(INT0_vect) {
+	portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
 	xSemaphoreGiveFromISR(xSemaphore, pdFALSE);
+	if (xHigherPriorityTaskWoken == pdTRUE) {
+		EIMSK = 0x00;			// Turns off INT0.
+	}
 }
